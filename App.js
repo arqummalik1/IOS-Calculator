f2dff602b3f40a17783e4b90009f7622b8935292
Arqum Malik <arqummalik1@gmail.com>
2022-10-24 18:31:17 +0530

    app

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Calculator
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
